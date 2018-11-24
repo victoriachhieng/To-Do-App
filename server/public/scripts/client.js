58b5c634aef9b78288dffe34071c3f57b438afe7
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-11-23 18:40:06 -0600

    set up server.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+console.log('js');
+
+$(document).ready(handleReady);
+
+function handleReady() {
+    console.log('in handleReady');
+    // Set up click listeners
+} // end handleReady
---
+++ server/server.js
@@ -0,0 +1,20 @@
+// Require
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 5000;
+
+// USES
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('server/public'));
+
+// GET
+app.get('/todo', (req, res) => {
+    console.log('in GET route');
+    res.send('HELLO');
+}); // END GET ROUTE
+
+// Start listening for requests on a specific port
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});
